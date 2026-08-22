0929960fd8cc13c3b108f63de06e4a1d74e33980
Zolehlin <wriotbro@gmail.com>
2026-08-22 20:42:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-// If you're reusing your existing Supabase project (the one from reel), paste the SAME values here.
-const SUPABASE_URL = "PASTE_YOUR_SUPABASE_PROJECT_URL_HERE";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_ANON_PUBLIC_KEY_HERE";
+const SUPABASE_URL = "https://tfobwkvrighqusotdhxm.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_titBMApwLKsTYyyKz81YPA_2m0OFwn-";
